440350ebb53e7ac2d569a567eb64e4611ede9b01
Fonoster Team <fonosterteam@fonoster.com>
2017-05-30 13:15:24 -0400

    Update sipio cookbook

--- chef-repo/cookbooks/fn_sipio/attributes/default.rb
+++ chef-repo/cookbooks/fn_sipio/attributes/default.rb
@@ -1,6 +1,6 @@
 # Sip I/O Config
-default['sipio']['downloadUrl']        = ''
-default['sipio']['file']               = ''
+default['sipio']['downloadUrl']        = 'https://github.com/psanders/sipio/releases/download/1.0.0-M1/sipio.1.0.0-M1.tar.gz'
+default['sipio']['filename']           = 'sipio.1.0.0-M1'
 default['sipio']['home']               = '/opt/sipio'
 default['sipio']['user']               = 'fonoster'
 default['sipio']['group']              = 'fonoster'
--- chef-repo/cookbooks/fn_sipio/recipes/default.rb
+++ chef-repo/cookbooks/fn_sipio/recipes/default.rb
@@ -1,6 +1,7 @@
 # Cooking Sip I/O
 
 include_recipe 'users'
+include_recipe 'java_se'
 
 users_manage "fonoster" do
     action [:remove, :create]
@@ -15,6 +16,7 @@ package 'wget' do
    action :upgrade
 end
 
+# Can't simply download this because is a private repo
 cookbook_file "/tmp/sipio-vendor-plugins-master.tar.gz" do
   source "sipio-vendor-plugins-master.tar.gz"
   action :create
@@ -24,30 +26,13 @@ end
 bash 'Installing Sip I/O Server' do
     cwd "/tmp"
     code <<-EOH
-        wget https://github.com/psanders/sip.io/archive/master.tar.gz
-        tar -xzvf master.tar.gz
-        cp -rf sipio-master/* #{node['sipio']['home']}
-
-        # Installing the vendor plugin
-        mkdir #{node['sipio']['home']}/mod/vendor
-        tar -xzvf sipio-vendor-plugins-master.tar.gz
-        mv sipio-vendor-plugins-master/fn_resources #{node['sipio']['home']}/mod/vendor
-
-        # Installing the JDK9
-        wget http://download.java.net/java/jdk9/archive/169/binaries/jdk-9-ea+169_linux-x64_bin.tar.gz
-        tar xvf jdk-9-ea+169_linux-x64_bin.tar.gz
-        mv jdk-9 #{node['sipio']['home']}
+        wget #{node['sipio']['downloadUrl']}
+        tar xvf #{node['sipio']['filename']}.tar.gz
+        mv #{node['sipio']['filename']} #{node['sipio']['home']}
+        # Maybe this should go to attributes
+        tar xvf sipio-vendor-plugins-master.tar.gz
+        mv sip-vendor-plugins-master/vendor #{node['sipio']['home']}/mod/
     EOH
-end
-
-template "#{node['sipio']['home']}/sipio" do
-  source 'sipio.erb'
-  mode '0755'
-end
-
-template "#{node['sipio']['home']}/sipioctl" do
-  source 'sipioctl.erb'
-  mode '0755'
 end
 
 template "#{node['sipio']['home']}/config/agents.yml" do
