179243267a0f72b6d2122eee7850c47ca51e582f
Fonoster Team <fonosterteam@fonoster.com>
2017-07-05 20:51:54 -0400

    Updater Sip I/O version

--- chef-repo/cookbooks/fn_sipio/attributes/default.rb
+++ chef-repo/cookbooks/fn_sipio/attributes/default.rb
@@ -1,6 +1,5 @@
 # Sip I/O Config
-default['sipio']['downloadUrl']        = 'https://github.com/psanders/sipio/releases/download/1.0.0-M1/sipio.1.0.0-M1.tar.gz'
-default['sipio']['filename']           = 'sipio.1.0.0-M1'
+default['sipio']['filename']           = 'sipio.1.0.0-M2'
 default['sipio']['home']               = '/opt/sipio'
 default['sipio']['user']               = 'fonoster'
 default['sipio']['group']              = 'fonoster'
