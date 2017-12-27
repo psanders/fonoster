819424d5465cd7eafcecf6480b2b1e3a36991651
Fonoster Team <fonosterteam@fonoster.com>
2017-12-27 09:19:17 -0500

    Updated asterisk cookbook...

--- chef-repo/cookbooks/fn_sipio/attributes/default.rb
+++ chef-repo/cookbooks/fn_sipio/attributes/default.rb
@@ -1,5 +1,5 @@
 # Sip I/O Config
-default['sipio']['filename']           = 'sipio.1.0.0-M2'
+default['sipio']['filename']           = 'sipio.1.0.0-M3'
 default['sipio']['home']               = '/opt/sipio'
 default['sipio']['user']               = 'fonoster'
 default['sipio']['group']              = 'fonoster'
