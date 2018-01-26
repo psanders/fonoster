ce514b026f7061028ea95195ea2aa011aa48018f
Fonoster Team <fonosterteam@fonoster.com>
2018-01-25 19:30:03 -0500

    Improved runner script

---
+++ chef-repo/cookbooks/fn_mongodb/files/default/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+repo=/var/chef-repo
+db=fonoster
+
+# Unrestricted while creating user
+mongod --config /etc/mongod.conf; sleep 3
+mongo $db /create-db-user.js
+
+# Bootstrap the service for developers
+if [ "$FONOSTER_ENV" = "dev" ]
+then
+    ./bootstrap.sh $repo/data_bags/bootstrap
+fi
+
+# Stop mongo. Allow for a complete shutdown. Then start in secure mode.
+mongo --eval "db.getSiblingDB('admin').shutdownServer()"; sleep 5
+mongod --auth --config /etc/mongod.conf
+
+echo 'Data server is up!'
+
+sleep infinity
